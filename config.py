fc38502247d7062c0a03a70f8a6478ec694d72a7
Akki Reddy <challaakkireddy@gmail.com>
2018-11-09 12:35:24 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,8 @@
 #!/usr/bin/python
 
-USERNAME = 'challaakkireddy@gmail.com'
-PASSWORD = '13026A0508'
+USERNAME = '..'
+PASSWORD = '...'
 
 COURSES = [
     'https://www.linkedin.com/learning/learning-java-3',
-    #'it-security-foundations-core-concepts',
-    #'charlene-li-on-digital-leadership',
-    # 'fred-kofman-on-managing-conflict',
-    # 'reid-hoffman-and-chris-yeh-on-creating-an-alliance-with-employees',
-    # 'bill-george-on-self-awareness-authenticity-and-leadership',
-    # 'sallie-krawcheck-on-risk-taking',
-    # 'betsy-corcoran-on-choosing-the-right-technology-for-your-school',
-    # 'betty-liu-on-career-success',
-    # 'j-t-o-donnell-on-making-recruiters-come-to-you',
-    # 'gary-hamel-on-busting-bureaucracy',
-    # 'shane-snow-on-storytelling',
-    # 'john-maeda-on-design-business-and-inclusion',
-    # 'esther-dyson-on-cultivating-health-at-scale',
-    # 'influencer-interview-oprah-winfrey'
 ]
